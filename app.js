c3bb3ee011275af6a22a85c85b7d32bfbd265a15
Dacio Romero <dacioromero@gmail.com>
2018-09-04 15:50:11 -0700

    Reformat, update .gitignore

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // ROUTES
-app.get('/hello-world', function (req, res) {
-    res.send('Hello World');
-});
+// app.get('/hello-world', function (req, res) {
+//     res.send('Hello World');
+// });
 
-app.get('/hello-squirrel', function (req, res) {
-    res.send('Hello Squirrel');
-});
+// app.get('/hello-squirrel', function (req, res) {
+//     res.send('Hello Squirrel');
+// });
 
 app.get('/', function (req, res) {
     if(req.query.term) {
@@ -32,15 +32,15 @@ app.get('/', function (req, res) {
     }
 });
 
-app.get('/hello-gif', function (req, res) {
-    var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
-    res.render('hello-gif', {gifUrl: gifUrl})
-})
+// app.get('/hello-gif', function (req, res) {
+//     var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+//     res.render('hello-gif', {gifUrl: gifUrl})
+// })
 
-app.get('/greetings/:name', function (req, res) {
-    var name = req.params.name;
-    res.render('greetings', {name: name});
-})
+// app.get('/greetings/:name', function (req, res) {
+//     var name = req.params.name;
+//     res.render('greetings', {name: name});
+// })
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
