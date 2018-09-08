66f78e5fff64fd340b3b1b67b6cae4e649748724
Dacio Romero <dacioromero@gmail.com>
2018-09-07 23:14:42 -0700

    Remove unused code and files

--- app.js
+++ app.js
@@ -1,32 +1,26 @@
-// DECLARATIONS
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var http    = require('http');
-var giphy   = require('giphy-api')()
-var path    = require('path')
-var app     = express();
+// DEPENDENCIES
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const http    = require('http');
+const giphy   = require('giphy-api')()
+const path    = require('path')
+
+const app = express();
 
 // MIDDLEWARE
 app.use(express.static('public'));
 
-app.engine('handlebars', exphbs({
+app.engine('hbs', exphbs({
+    extname: '.hbs',
     layoutsDir: path.join(__dirname, '/views/layouts/'),
     partialsDir: path.join(__dirname, '/views/partials/'),
     defaultLayout: 'main'
 }));
 
 app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 
 // ROUTES
-// app.get('/hello-world', function (req, res) {
-//     res.send('Hello World');
-// });
-
-// app.get('/hello-squirrel', function (req, res) {
-//     res.send('Hello Squirrel');
-// });
-
 app.get('/', function (req, res) {
     if(req.query.term) {
         giphy.search(req.query.term || "cat", function (err, response) {
@@ -40,21 +34,12 @@ app.get('/', function (req, res) {
     }
 });
 
-// app.get('/hello-gif', function (req, res) {
-//     var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
-//     res.render('hello-gif', {gifUrl: gifUrl})
-// })
-
-// app.get('/greetings/:name', function (req, res) {
-//     var name = req.params.name;
-//     res.render('greetings', {name: name});
-// })
-
+// LISTENER - only if directly run
 if (require.main === module) {
     let port = process.env.PORT || 3000;
 
     app.listen(port, () => {
-        console.log('App listening on port ' + port + '!');
+        console.log(`App listening on port ${port}!`);
     });
 }
 
